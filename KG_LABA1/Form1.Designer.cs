c591573284f532e423a9dad4267af351a045d343
LonelyDragon <illialubenets@yandex.ua>
2016-05-02 01:09:06 +0300

    Remove  information tab

--- KG_LABA1/Form1.Designer.cs
+++ KG_LABA1/Form1.Designer.cs
@@ -33,22 +33,22 @@
             this.dDAЛініяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.несиметричнаDDAЛініяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.модифікованийАлгортимБрезенхемаГенеруванняЛінійToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.алгоритмВуГенеруванняЛініїToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.прізвищеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.проАвтораToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.вибратиМалюнокToolStripMenuItem,
-            this.проАвтораToolStripMenuItem});
+            this.вибратиМалюнокToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(748, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(997, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -62,66 +62,60 @@
             this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem,
             this.прізвищеToolStripMenuItem});
             this.вибратиМалюнокToolStripMenuItem.Name = "вибратиМалюнокToolStripMenuItem";
-            this.вибратиМалюнокToolStripMenuItem.Size = new System.Drawing.Size(120, 20);
+            this.вибратиМалюнокToolStripMenuItem.Size = new System.Drawing.Size(148, 24);
             this.вибратиМалюнокToolStripMenuItem.Text = "Вибрати малюнок";
             // 
             // dDAЛініяToolStripMenuItem
             // 
             this.dDAЛініяToolStripMenuItem.Name = "dDAЛініяToolStripMenuItem";
-            this.dDAЛініяToolStripMenuItem.Size = new System.Drawing.Size(388, 22);
+            this.dDAЛініяToolStripMenuItem.Size = new System.Drawing.Size(485, 26);
             this.dDAЛініяToolStripMenuItem.Text = "DDA лінія";
             this.dDAЛініяToolStripMenuItem.Click += new System.EventHandler(this.dDAЛініяToolStripMenuItem_Click);
             // 
             // несиметричнаDDAЛініяToolStripMenuItem
             // 
             this.несиметричнаDDAЛініяToolStripMenuItem.Name = "несиметричнаDDAЛініяToolStripMenuItem";
-            this.несиметричнаDDAЛініяToolStripMenuItem.Size = new System.Drawing.Size(388, 22);
+            this.несиметричнаDDAЛініяToolStripMenuItem.Size = new System.Drawing.Size(485, 26);
             this.несиметричнаDDAЛініяToolStripMenuItem.Text = "Несиметрична DDA лінія ";
             this.несиметричнаDDAЛініяToolStripMenuItem.Click += new System.EventHandler(this.несиметричнаDDAЛініяToolStripMenuItem_Click);
             // 
             // модифікованийАлгортимБрезенхемаГенеруванняЛінійToolStripMenuItem
             // 
             this.модифікованийАлгортимБрезенхемаГенеруванняЛінійToolStripMenuItem.Name = "модифікованийАлгортимБрезенхемаГенеруванняЛінійToolStripMenuItem";
-            this.модифікованийАлгортимБрезенхемаГенеруванняЛінійToolStripMenuItem.Size = new System.Drawing.Size(388, 22);
+            this.модифікованийАлгортимБрезенхемаГенеруванняЛінійToolStripMenuItem.Size = new System.Drawing.Size(485, 26);
             this.модифікованийАлгортимБрезенхемаГенеруванняЛінійToolStripMenuItem.Text = "Модифікований алгортим Брезенхема генерування ліній";
             this.модифікованийАлгортимБрезенхемаГенеруванняЛінійToolStripMenuItem.Click += new System.EventHandler(this.модифікованийАлгортимБрезенхемаГенеруванняЛінійToolStripMenuItem_Click);
-            // 
-            // алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem
-            // 
-            this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem.Name = "алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem";
-            this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem.Size = new System.Drawing.Size(388, 22);
-            this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem.Text = "Алгоритм Брезенхема генерування кола";
-            this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem.Click += new System.EventHandler(this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem_Click);
             // 
             // алгоритмВуГенеруванняЛініїToolStripMenuItem
             // 
             this.алгоритмВуГенеруванняЛініїToolStripMenuItem.Name = "алгоритмВуГенеруванняЛініїToolStripMenuItem";
-            this.алгоритмВуГенеруванняЛініїToolStripMenuItem.Size = new System.Drawing.Size(388, 22);
+            this.алгоритмВуГенеруванняЛініїToolStripMenuItem.Size = new System.Drawing.Size(485, 26);
             this.алгоритмВуГенеруванняЛініїToolStripMenuItem.Text = "Алгоритм Ву генерування лінії";
             this.алгоритмВуГенеруванняЛініїToolStripMenuItem.Click += new System.EventHandler(this.алгоритмВуГенеруванняЛініїToolStripMenuItem_Click);
+            // 
+            // алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem
+            // 
+            this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem.Name = "алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem";
+            this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem.Size = new System.Drawing.Size(485, 26);
+            this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem.Text = "Алгоритм Брезенхема генерування кола";
+            this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem.Click += new System.EventHandler(this.алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem_Click);
             // 
             // прізвищеToolStripMenuItem
             // 
             this.прізвищеToolStripMenuItem.Name = "прізвищеToolStripMenuItem";
-            this.прізвищеToolStripMenuItem.Size = new System.Drawing.Size(388, 22);
+            this.прізвищеToolStripMenuItem.Size = new System.Drawing.Size(485, 26);
             this.прізвищеToolStripMenuItem.Text = "Прізвище";
             this.прізвищеToolStripMenuItem.Click += new System.EventHandler(this.прізвищеToolStripMenuItem_Click);
             // 
-            // проАвтораToolStripMenuItem
-            // 
-            this.проАвтораToolStripMenuItem.Name = "проАвтораToolStripMenuItem";
-            this.проАвтораToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
-            this.проАвтораToolStripMenuItem.Text = "Інформація";
-            this.проАвтораToolStripMenuItem.Click += new System.EventHandler(this.проАвтораToolStripMenuItem_Click);
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(748, 439);
+            this.ClientSize = new System.Drawing.Size(997, 540);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
@@ -141,7 +135,6 @@
         private System.Windows.Forms.ToolStripMenuItem алгоритмБрезенхемаГенеруванняКолаToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem алгоритмВуГенеруванняЛініїToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem прізвищеToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem проАвтораToolStripMenuItem;
 
 
     }
